ba82f778041106bbc9b679e043818d0e66583fab
Zane Helton <zane99@me.com>
2023-07-05 16:45:39 -0400

    Refactor to is_overlap?()

--- AOC/four/lib/four.ex
+++ AOC/four/lib/four.ex
@@ -10,21 +10,24 @@ defmodule Four do
     read_file()
     |> parse_range_pairs()
     |> Enum.filter(fn range_pair ->
-      is_full_overlap?(range_pair)
+      is_overlap?(range_pair, &Enum.all?/2)
     end)
-    |> length
+    |> length()
   end
 
   def solve(:second) do
     read_file()
     |> parse_range_pairs()
     |> Enum.filter(fn range_pair ->
-      is_partial_overlap?(range_pair)
+      is_overlap?(range_pair)
     end)
     |> length()
   end
 
-  def is_partial_overlap?([range_1, range_2]) do
+  def is_overlap?(
+        [range_1, range_2],
+        fun \\ &Enum.any?/2
+      ) do
     [range_1_start, range_1_end] = range_1
     [range_2_start, range_2_end] = range_2
 
@@ -37,33 +40,16 @@ defmodule Four do
       |> Range.to_list()
 
     one_has_two =
-      Enum.any?(range_list_1, fn r ->
+      fun.(range_list_1, fn r ->
         not is_nil(Enum.find_index(range_list_2, &(&1 == r)))
       end)
 
     two_has_one =
-      Enum.any?(range_list_2, fn r ->
+      fun.(range_list_2, fn r ->
         not is_nil(Enum.find_index(range_list_1, &(&1 == r)))
       end)
 
     one_has_two or two_has_one
-  end
-
-  def is_full_overlap?([range_1, range_2]) do
-    [range_1_start, range_1_end] = range_1
-    [range_2_start, range_2_end] = range_2
-
-    if range_1_start >= range_2_start and
-         range_1_end <= range_2_end do
-      true
-    else
-      if range_2_start >= range_1_start and
-           range_2_end <= range_1_end do
-        true
-      else
-        false
-      end
-    end
   end
 
   def parse_range_pairs(input) do
